8a296abe4ebe58850ebe8c7d9de1ea6cebe029fd
jeremygill <73575732+jeremygill@users.noreply.github.com>
2025-09-30 15:15:31 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ function setup() {
         { url: this.dialNumber, name: 'Call IT Support' },
         { url: this.dialNumber, name: 'Advice' },
         { url: this.dialNumber, name: 'Credit' },
-        { url: this.instructions, name: 'Instructions', page: 'instructions'},
+        { name: 'Instructions', page: 'instructions'},
       ];
     },
     get page() {
