d02f917588d6f6d81ca7f6904752ed486da5294c
jeremygill <73575732+jeremygill@users.noreply.github.com>
2025-10-01 14:43:07 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
         TrackingData: 'WebrtcJoin_command',
         Type: 'MSTeams',
         Url: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZjkzYWVjOWMtNTRhMy00MjgyLWEwZjAtOTM0MTgxM2RhNmMw%40thread.v2/0?context=%7b%22Tid%22%3a%2293b0ec50-fc3f-47ed-9345-9ffc6012541c%22%2c%22Oid%22%3a%22f4191fbd-c7c1-4fa0-8679-d380e280aebf%22%7d'
-    }
+    
 });
     },
     get page() {
@@ -53,6 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.addEventListener('alpine:init', setup);
+
 
 
 
